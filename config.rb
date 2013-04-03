8ba47196bd5d57bd6975e2b31a441819fe1ac1fa
jwebcat <jwebcat@gmail.com>
2013-04-02 20:39:29 -0700

    make compass config pretty

--- config.rb
+++ config.rb
@@ -1,13 +1,23 @@
 # Require any additional compass plugins here.
+# -----------------------------------------------------------------------------
 
 
 # Set this to the root of your project when deployed:
+# -----------------------------------------------------------------------------
+
 http_path = "/"
 sass_dir = "_sass"
 css_dir = "_site/assets/stylesheets"
 javascripts_dir = "assets/javascripts"
 images_dir = "assets/images"
 fonts_dir = "assets/fonts"
+# svg_dir = "assets/svg"
+# docs_dir = "assets/docs"
+# plugins_dir = "assets/plugins"
+
+
+# Output style and comments
+# -----------------------------------------------------------------------------
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -18,6 +28,15 @@ fonts_dir = "assets/fonts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
+# SASS core
+# -----------------------------------------------------------------------------
+
+# Chrome needs a precision of 7 to round properly
+Sass::Script::Number.precision = 7
+
+
+# Stuff we don't really need below
+# -----------------------------------------------------------------------------
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
